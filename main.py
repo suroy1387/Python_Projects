7a539f2c901e381547abb852b9dfeba443e66570
SUBHANKAR ROY <109498159+suroy1387@users.noreply.github.com>
2022-08-02 04:49:34 +0000

    In production of While loop and code to introduce options for Yes and No to continue using the tool

--- main.py
+++ main.py
@@ -27,13 +27,19 @@ print(art.logo)
 #If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 #Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+should_continue=True
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to     decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
 
 #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
 #Try running the program and entering a shift number of 45.
 #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
 #Hint: Think about how you can use the modulus (%).
-shift=shift%26
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  shift=shift%26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  result=input('Type YES is you wangt to continue or NO if want to leave \n').lower()
+  if result=='no':
+    should_continue=False
+    print('Thanks for using Ceasar-cipher! Goodbye.')
